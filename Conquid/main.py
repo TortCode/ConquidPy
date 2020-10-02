6c897457b1bfee5ff1038185d9a851d7b193af63
TortCode <44101297+TortCode@users.noreply.github.com>
2020-10-02 13:38:38 -0400

    Update main.py

--- Conquid/main.py
+++ Conquid/main.py
@@ -6,18 +6,18 @@ from boardview import BoardView
 from controller import Controller
 import json
 
-
+#create new game
 def newcmd():
     rows = simpledialog.askinteger('Input', 'How many rows?', minvalue=3, parent=root)
     cols = simpledialog.askinteger('Input', 'How many columns?', minvalue=14, parent=root)
     bdv.discard_tiles()
     load_history(make_history(rows,cols))
-
+#save current game
 def saveascmd():
     save_file = filedialog.asksaveasfilename()
     with open(save_file, 'w') as sf:
         json.dump(cache.hist.__dict__, sf)
-
+#open saved game
 def opencmd():
     open_file = filedialog.askopenfilename()
     with open(open_file) as of:
@@ -38,7 +38,7 @@ def load_history(history):
     root.geometry(str(max(board.cols*40,800))+'x'+str(board.rows*40)+'+400+200')
     bdv.setup(controller, board.rows, board.cols)
     cache.link_gui(controller, bdv)
-
+#set colors of display
 def set_color(player, base=False):
     rgb, color = colorchooser.askcolor()
     if base:
@@ -74,7 +74,7 @@ controller = Controller()
 controller.boardview = bdv
 load_history(make_history(15, 30))
 
-#make buttons
+#move buttons
 move_btns = {}
 move_btns['A'] = tk.Button(button_frame, relief='groove', text='acquire', width=8, command=lambda:controller.button_pressed('A'))
 root.bind('<a>', lambda e: move_btns['A'].invoke())
@@ -84,18 +84,19 @@ move_btns['V'] = tk.Button(button_frame, relief='groove', text='vanquish', width
 root.bind('<v>', lambda e: move_btns['V'].invoke())
 move_btns['Q'] = tk.Button(button_frame, relief='groove', text='conquest', width=8, command=lambda:controller.button_pressed('Q'))
 root.bind('<q>', lambda e: move_btns['Q'].invoke())
+#undo and confirm
 undo_btn = tk.Button(button_frame, relief='groove', text='undo', width=8, command=controller.undo)
 root.bind('<u>', lambda e: undo_btn.invoke())
 confirm_btn = tk.Button(button_frame,relief='groove', text='confirm', width=8, command=controller.confirm)
 root.bind('<Return>', lambda e: confirm_btn.invoke())
-
+#playback
 prev_btn = tk.Button(button_frame,relief='groove', text='<<', width=10, command=controller.prev_board)
 root.bind('<Left>', lambda e: prev_btn.invoke())
 pause_play = tk.Button(button_frame,relief='groove', text='#', width=10, command=controller.pauseplay)
 root.bind('<Up>', lambda e: pause_play.invoke())
 next_btn = tk.Button(button_frame,relief='groove', text='>>', width=10, command=controller.next_board)
 root.bind('<Right>', lambda e: next_btn.invoke())
-
+#link to controller
 controller.link_buttons(move_btns, undo_btn, confirm_btn, prev_btn, pause_play, next_btn)
 
 #pack
@@ -110,6 +111,6 @@ confirm_btn.grid(row=0, column=6)
 prev_btn.grid(row=0, column=7)
 pause_play.grid(row=0,column=8)
 next_btn.grid(row=0, column=9)
-
+#rev it up
 root.focus()
 root.mainloop()
